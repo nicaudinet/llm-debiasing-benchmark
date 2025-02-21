74bb77369e07a64e192ffd6efdb41bc09195ed74
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2025-02-21 13:41:05 +0100

    Update vera script

--- exe/cluster/run-vera.sh
+++ exe/cluster/run-vera.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #SBATCH --job-name=dsl-use-simulation
-#SBATCH --account=C3SE2024-1-17
+#SBATCH --account=C3SE2025-1-14
 #SBATCH --partition=vera
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
-#SBATCH --array=1-1000
+#SBATCH --array=1-2
 
-#SBATCH --time=1-00:00:00
+#SBATCH --time=0-01:00:00
 #SBATCH --mail-user=nicolas.audinet@chalmers.se
 #SBATCH --mail-type=all
 #SBATCH --output=results/run2/logs/output/output_%A_%a.log
