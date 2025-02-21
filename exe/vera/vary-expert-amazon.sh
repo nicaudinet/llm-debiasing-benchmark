5578a19efcdda56b3e1373c688f25d92293f20e4
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2025-02-21 14:35:27 +0100

    Very expert amazon

---
+++ exe/vera/vary-expert-amazon.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH --job-name=dsl-use-simulation
+#SBATCH --account=C3SE2025-1-14
+#SBATCH --partition=vera
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --array=1-500
+#SBATCH --time=0-00:15:00
+
+#SBATCH --mail-user=nicolas.audinet@chalmers.se
+#SBATCH --mail-type=all
+#SBATCH --output=results/logs/output_%A_%a.log
+#SBATCH --error=results/logs/error_%A_%a.log
+
+module purge
+module load rpy2
+module load scikit-learn/1.4.2-gfbf-2023a
+
+annotated_reviews="/cephyr/users/audinet/Vera/datasets/amazon/annotated_reviews.pkl"
+base_dir="/cephyr/users/audinet/Vera/dsl-use"
+result_dir="$base_dir/results/vary-num-expert/amazon"
+data_dir="$result_dir/data"
+plot_dir="$result_dir/plots"
+
+mkdir -p $data_dir
+mkdir -p $plot_dir
+
+python \
+    "$base_dir/lib/vary_expert_realworld.py" \
+    $annotated_reviews \
+    "$data_dir/data_amazon_$i.npz"
+
+# python3.12 "$base_dir/lib/vary_expert_plot.py" $data_dir $plot_dir
