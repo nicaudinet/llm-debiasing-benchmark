9456b9ac094bb93826c9a9277a7a489581a6996b
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2025-02-25 15:54:52 +0100

    Script to copy plots from Vera

---
+++ exe/scp_plots_vera.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+function scp_plot_expert () {
+    DATASET=$1
+    RESULT_DIR="results/vera/vary-num-expert/$DATASET"
+    mkdir -p $RESULT_DIR
+    scp -r vera:~/dsl-use/results/vary-num-expert/$DATASET/plot $RESULT_DIR
+}
+
+scp_plot_expert "simulation"
+scp_plot_expert "amazon"
+scp_plot_expert "misinfo"
+
+function scp_plot_total () {
+    DATASET=$1
+    RESULT_DIR="results/vera/vary-num-total/$DATASET"
+    mkdir -p $RESULT_DIR
+    scp -r vera:~/dsl-use/results/vary-num-total/$DATASET/plot $RESULT_DIR
+}
+
+scp_plot_total "simulation"
+scp_plot_total "amazon"
+scp_plot_total "misinfo"
