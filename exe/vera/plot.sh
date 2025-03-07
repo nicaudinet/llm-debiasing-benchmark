c02a1a61ffca4d16ea3f79e7cbe772a15abd35c3
Nicolas Pietro Marie Audinet de Pieuchon <audinet@vera1.int.private>
2025-03-07 16:47:59 +0100

    Update plot script to use mimer paths

--- exe/vera/plot.sh
+++ exe/vera/plot.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-BASE_DIR="/cephyr/users/audinet/Vera/dsl-use/"
+HOME_DIR="/cephyr/users/audinet/Vera/dsl-use"
+BASE_DIR="/mimer/NOBACKUP/groups/ci-nlp-alvis/dsl-use/experiments"
 
 module purge
 module load matplotlib/3.7.2-gfbf-2023a
@@ -11,12 +12,12 @@ module load matplotlib/3.7.2-gfbf-2023a
 
 function plot_expert () {
     local DATASET=$1
-    DATA_DIR="$BASE_DIR/results/vary-num-expert/$DATASET/data"
+    DATA_DIR="$BASE_DIR/vary-num-expert/$DATASET/data"
     if [ -d $DATA_DIR ]; then
         echo -e "\nVary expert ($DATASET)"
-        PLOT_DIR="$BASE_DIR/results/vary-num-expert/$DATASET/plot"
+        PLOT_DIR="$BASE_DIR/vary-num-expert/$DATASET/plot"
         mkdir -p $PLOT_DIR
-        python "$BASE_DIR/lib/vary_expert_plot.py" $DATA_DIR $PLOT_DIR
+        python "$HOME_DIR/lib/vary_expert_plot.py" $DATA_DIR $PLOT_DIR
     else
         echo -e "\nVary num expert ($DATASET) data dir not found"
     fi
@@ -34,14 +35,14 @@ plot_expert "germeval"
 
 function plot_total () {
     local DATASET=$1
-    DATA_DIR="$BASE_DIR/results/vary-num-total/$DATASET/data"
+    DATA_DIR="$BASE_DIR/vary-num-total/$DATASET/data"
     if [ -d $DATA_DIR ]; then
         for n in 200 1000 5000; do
         echo -e "\nVary total n$n ($DATASET)"
         DATA_DIR_N="$DATA_DIR/n$n"
-            PLOT_DIR="$BASE_DIR/results/vary-num-total/$DATASET/plot"
+            PLOT_DIR="$BASE_DIR/vary-num-total/$DATASET/plot"
             mkdir -p $PLOT_DIR
-            python "$BASE_DIR/lib/vary_total_plot.py" $DATA_DIR_N $PLOT_DIR $n
+            python "$HOME_DIR/lib/vary_total_plot.py" $n $DATA_DIR_N $PLOT_DIR
         done
     else
         echo -e "\nVary num total ($DATASET) data dir not found"
