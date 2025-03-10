34e491b84c30a9496c2625f3839345d0ad6f7586
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2025-03-10 15:29:09 +0100

    Update scp plots script

--- exe/scp_plots_vera.sh
+++ exe/scp_plots_vera.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
 
+MIMER_PATH="/mimer/NOBACKUP/groups/ci-nlp-alvis/dsl-use/experiments"
+
+echo ""
+echo "-------------------------"
+echo "-- Vary Expert Samples --"
+echo "-------------------------"
+echo ""
+
 function scp_plot_expert () {
     DATASET=$1
+    echo ""
+    echo "Copying plots from $DATASET"
     RESULT_DIR="results/vera/vary-num-expert/$DATASET"
     mkdir -p $RESULT_DIR
-    scp -r vera:~/dsl-use/results/vary-num-expert/$DATASET/plot $RESULT_DIR
+    scp -r vera:$MIMER_PATH/vary-num-expert/$DATASET/plot_deepseek $RESULT_DIR
 }
 
 scp_plot_expert "simulation"
@@ -13,11 +23,19 @@ scp_plot_expert "misinfo"
 scp_plot_expert "biobias"
 scp_plot_expert "germeval"
 
+echo ""
+echo "------------------------"
+echo "-- Vary Total Samples --"
+echo "------------------------"
+echo ""
+
 function scp_plot_total () {
     DATASET=$1
+    echo ""
+    echo "Copying plots from $DATASET"
     RESULT_DIR="results/vera/vary-num-total/$DATASET"
     mkdir -p $RESULT_DIR
-    scp -r vera:~/dsl-use/results/vary-num-total/$DATASET/plot $RESULT_DIR
+    scp -r vera:$MIMER_PATH/vary-num-total/$DATASET/plot_deepseek $RESULT_DIR
 }
 
 scp_plot_total "simulation"
