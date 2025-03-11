f3f9595c96c5709e9b826d26cb5424443586e171
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2025-03-11 16:04:03 +0100

    Annotate LLaMa update

--- lib/annotate_llama.py
+++ lib/annotate_llama.py
@@ -1,5 +1,25 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from annotate_prompts import system_prompts, make_user_prompt
+from argparse import ArgumentParser
+from pathlib import Path
+import pandas as pd
+from itertools import batched
+import os
+
+###########
+# Options #
+###########
+
+parser = ArgumentParser()
+parser.add_argument('dataset', choices = list(system_prompts.keys()))
+parser.add_argument('parsed_path', type = Path)
+parser.add_argument('annotation_dir', type = Path)
+parser.add_argument('--num', type = int, default = 5)
+parser.add_argument('--model', type = str, default = "meta-llama/Llama-3.2-3B-Instruct")
+parser.add_argument('--batchsize', type = int, default = 8)
+args = parser.parse_args()
+print(f"Running {args.num} DeepSeek API requests")
 
 device = "cpu"
 if torch.cuda.is_available():
@@ -8,26 +28,68 @@ elif torch.mps.is_available():
     device = "mps"
 print("Using device:", device)
 
-model_id = "meta-llama/Llama-3.2-3B-Instruct"
+#################
+# Load the data #
+#################
 
-print(f"Loading tokenizer for {model_id}")
-tokenizer = AutoTokenizer.from_pretrained(model_id)
+data = pd.read_json(args.parsed_path)
+data = data[:args.num]
+data = data.reset_index(drop = True)
 
-print(f"Loading model for {model_id}")
+############
+# Annotate #
+############
+
+data["prompt"] = data["text"].apply(lambda x: make_user_prompt(args.dataset, x))
+print(data)
+
+print(f"Loading tokenizer for {args.model}")
+tokenizer = AutoTokenizer.from_pretrained(args.model)
+
+print(f"Loading model for {args.model}")
 model = AutoModelForCausalLM.from_pretrained(
-    model_id,
+    args.model,
     device_map = device,
     torch_dtype = torch.float16,
 )
 
-pipe = pipeline(
+generator = pipeline(
     "text-generation",
     model = model,
     tokenizer = tokenizer,
     max_new_tokens = 512,
+    num_return_sequences = 1,
 )
 
-user_input = "why is the sea red?"
-full_response = pipe(user_input)[0]["generated_text"]
-model_response = full_response[len(user_input):].strip()
-print("\nLlama:", model_response)
+def make_chat(text):
+    return [
+        {
+            "role": "system",
+            "content":system_prompts[args.dataset],
+        },
+        {
+            "role": "user",
+            "content": make_user_prompt(args.dataset, text),
+        },
+    ]
+
+for batch in batched(range(len(data)), args.batchsize):
+    chats = [make_chat(text) for text in data["text"][batch]]
+    outputs = generator(chats)
+    print(outputs)
+    with open("outputs.txt", "r", encoding = "utf-8") as file:
+        file.write(str(outputs))
+
+def save_prompt(annotation_path, prompt, index):
+    prompt_path = annotation_path / Path("prompts")
+    os.makedirs(prompt_path, exist_ok = True)
+    prompt_file = prompt_path / Path(f"prompt_{index:04}.txt")
+    with open(prompt_file, "w", encoding="utf-8") as file:
+        file.write(prompt)
+
+def save_response(annotation_path, response, index):
+    response_path = annotation_path / Path("responses")
+    os.makedirs(response_path, exist_ok = True)
+    response_file = response_path / Path(f"response_{index:04}.txt")
+    with open(response_file, "w", encoding="utf-8") as file:
+        file.write(response)
