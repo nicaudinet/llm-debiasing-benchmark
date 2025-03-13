4493d47a89461ced643753548b6c1c41bb9c1986
Nicolas Pietro Marie Audinet de Pieuchon <audinet@vera1.int.private>
2025-03-13 13:21:32 +0100

    Cluster env stuff for gather phi4

--- exe/annotation/phi4.sh
+++ exe/annotation/phi4.sh
@@ -2,6 +2,11 @@
 
 set -eo pipefail
 
+module purge
+module load Python/3.12.3-GCCcore-13.3.0
+
+source /mimer/NOBACKUP/groups/ci-nlp-alvis/dsl-use/venv_alvis/bin/activate
+
 echo ""
 echo "##########"
 echo "# Gather #"
