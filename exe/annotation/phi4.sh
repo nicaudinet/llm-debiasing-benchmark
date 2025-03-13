71094d31682fa3396be03775761b6e61add0752b
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2025-03-13 13:07:06 +0100

    Gather Phi-4

---
+++ exe/annotation/phi4.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -eo pipefail
+
+echo ""
+echo "##########"
+echo "# Gather #"
+echo "##########"
+echo ""
+
+MIMER="/mimer/NOBACKUP/groups/ci-nlp-alvis/dsl-use/annotations"
+
+function gather () {
+
+    local DATASET=$1
+    local ANN_DIR="$MIMER/$DATASET"
+
+    mkdir -p "$ANN_DIR/phi4"
+
+    echo ""
+    echo "Gathering responses for $DATASET"
+    echo ""
+
+    python3 lib/gather_responses.py \
+        "$DATASET" \
+        "$ANN_DIR/parsed.json" \
+        "$ANN_DIR/phi4" \
+        "$ANN_DIR/annotated_phi4.json"
+}
+
+gather "amazon"
+gather "misinfo"
+gather "biobias"
+gather "germeval"
