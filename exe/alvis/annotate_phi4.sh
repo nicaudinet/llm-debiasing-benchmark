8dfebe7874b60ae879e55117b6f7804909d72a0f
Nicolas Audinet de Pieuchon <nicolas.audinet@chalmers.se>
2025-03-18 10:00:06 +0100

    Annotate Phi-4 50ex

--- exe/alvis/annotate_phi4.sh
+++ exe/alvis/annotate_phi4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=phi4-annotate
+#SBATCH --job-name=phi4-50ex
 #SBATCH --account=NAISS2025-22-180
 #SBATCH --partition=alvis
 
@@ -9,13 +9,15 @@
 #SBATCH --gpus-per-node=A40:1
 #SBATCH --time=0-06:00:00
 
-#SBATCH --output=logs/phi4_10ex/output_%A_%a.log
-#SBATCH --error=logs/phi4_10ex/error_%A_%a.log
+#SBATCH --output=logs/phi4_50ex/output_%A_%a.log
+#SBATCH --error=logs/phi4_50ex/error_%A_%a.log
 
 #SBATCH --mail-user=nicolas.audinet@chalmers.se
 #SBATCH --mail-type=all
 
 DATASET=$1
+
+echo "Dataset: $DATASET"
 
 module purge
 module load Python/3.12.3-GCCcore-13.3.0
@@ -24,7 +26,7 @@ source /mimer/NOBACKUP/groups/ci-nlp-alvis/dsl-use/venv_alvis/bin/activate
 
 BASE_DIR="/cephyr/users/audinet/Alvis/dsl-use"
 MIMER_PATH="/mimer/NOBACKUP/groups/ci-nlp-alvis/dsl-use/"
-ANN_DIR="$MIMER_PATH/annotations/$DATASET/phi4_10ex"
+ANN_DIR="$MIMER_PATH/annotations/$DATASET/phi4_50ex"
 
 mkdir -p $ANN_DIR
 
@@ -33,6 +35,5 @@ python3 "$BASE_DIR/lib/annotate_alvis.py" \
 	"$MIMER_PATH/annotations/$DATASET/parsed.json" \
 	"$ANN_DIR" \
 	--model "microsoft/phi-4" \
-	--num 8640 \
-	--start 1360 \
-	--num_examples 10
+	--num 10050 \
+	--num_examples 50
