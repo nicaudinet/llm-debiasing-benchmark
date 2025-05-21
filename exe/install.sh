8d8857f7d048ee46d4eedc5e3fd6c496cc767f94
Nicolas Pietro Marie Audinet de Pieuchon <audinet@vera1.int.private>
2025-05-21 14:16:19 +0200

    Update the install script

--- exe/install.sh
+++ exe/install.sh
@@ -10,8 +10,7 @@ echo "-- Loading modules --"
 echo "---------------------"
 
 module purge
-module load rpy2
-module load scikit-learn/1.4.2-gfbf-2023a
+module load rpy2 scikit-learn/1.4.2-gfbf-2023a
 
 echo "-----------------------"
 echo "-- Installing R libs --"
@@ -19,6 +18,14 @@ echo "-----------------------"
 
 R -e "install.packages(\"devtools\", repos=\"https://cloud.r-project.org/\")"
 R -e "library(devtools); install_github(\"naoki-egami/dsl\", dependencies=TRUE)"
+
+echo "-------------------------"
+echo "-- Installing the venv --"
+echo "-------------------------"
+
+virtualenv --system-site-packages /mimer/NOBACKUP/groups/ci-nlp-alvis/dsl-use/venv/vera
+source /mimer/NOBACKUP/groups/ci-nlp-alvis/dsl-use/venv/vera
+python -m pip install --no-cache-dir -r requirements_cluster.txt
 
 echo "-------------------------"
 echo "-- Running test script --"
