f5a9ddb14394e87569debd2fb212b08305f16a81
Nicolas Pietro Marie Audinet de Pieuchon <audinet@vera1.int.private>
2025-05-21 14:32:07 +0200

    Cleanup

--- exe/pip_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-module purge
-module load Python/3.12.3-GCCcore-13.3.0
-source /mimer/NOBACKUP/groups/ci-nlp-alvis/dsl-use/venv_alvis/bin/activate
-python -m pip install --no-cache-dir -r requirements_cluster.txt
